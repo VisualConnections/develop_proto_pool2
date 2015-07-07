e74f991f2eeeb2679cd6c16e90c0e7fc1d3cbc0d
CodeDeploy <CodeDeploy@visualconnections.net>
2015-07-06 22:01:41 -0400

    Test script commit
    Test script commit

--- public/modules/cards/tests/cards.client.controller.test.js
+++ public/modules/cards/tests/cards.client.controller.test.js
@@ -1,58 +1,66 @@
-'use strict';
+'use strict'
 
-(function() {
-	// Cards Controller Spec
-	describe('Cards Controller Tests', function() {
-		// Initialize global variables
-		var CardsController,
-			scope,
-			$httpBackend,
-			$stateParams,
-			$location;
+describe('Cards controller tests', function(){
 
-		// The $resource service augments the response object with methods for updating and deleting the resource.
-		// If we were to use the standard toEqual matcher, our tests would fail because the test values would not match
-		// the responses exactly. To solve the problem, we define a new toEqualData Jasmine matcher.
-		// When the toEqualData matcher compares two objects, it takes only object properties into
-		// account and ignores methods.
-		beforeEach(function() {
-			jasmine.addMatchers({
-				toEqualData: function(util, customEqualityTesters) {
-					return {
-						compare: function(actual, expected) {
-							return {
-								pass: angular.equals(actual, expected)
-							};
-						}
-					};
-				}
-			});
+	beforeEach(module('cards'));
+
+	var CardsCtrl,
+		scope;
+
+	beforeEach(inject(function($controller, $rootScope){
+
+		scope = $rootScope.$new();
+		CardsCtrl = $controller('CardsController', {
+			$scope: scope
+		});
+	}))
+
+	describe('Method: getCardsFromAPIs()', function() {
+
+		it('should have a function named $getCardsFromAPIs()', function() {
+			expect(angular.isFunction($scope.getCardsFromAPIs)).toBe(true);
+		});
+	});
+	describe('Method: getInitialCards()', function() {
+
+		it('should have a function named $getInitialCards()', function() {
+			expect(angular.isFunction($scope.getInitialCards)).toBe(true);
+		});
+	});
+	describe('Method: addCardsToMixedCardCollection()', function() {
+
+		it('should have a function named $addCardsToMixedCardCollection()', function() {
+			expect(angular.isFunction($scope.addCardsToMixedCardCollection)).toBe(true);
+		});
+	});
+	describe('Method: addFDADetails()', function() {
+
+		it('should have a function named $addFDADetails()', function() {
+			expect(angular.isFunction($scope.addFDADetails)).toBe(true);
 		});
 
-		// Then we can start by loading the main application module
-		beforeEach(module(ApplicationConfiguration.applicationModuleName));
+	describe('Method: shuffleCards()', function() {
 
-		// The injector ignores leading and trailing underscores here (i.e. _$httpBackend_).
-		// This allows us to inject a service but then attach it to a variable
-		// with the same name as the service.
-		beforeEach(inject(function($controller, $rootScope, _$location_, _$stateParams_, _$httpBackend_) {
-			// Set a new global scope
-			scope = $rootScope.$new();
+		it('should have a function named $shuffleCards()', function() {
+			expect(angular.isFunction($scope.shuffleCards)).toBe(true);
+		});
 
-			// Point global variables to injected services
-			$stateParams = _$stateParams_;
-			$httpBackend = _$httpBackend_;
-			$location = _$location_;
+	describe('Method: outputMixedCards()', function() {
 
-			// Initialize the Cards controller.
-			CardsController = $controller('CardsController', {
-				$scope: scope
-			});
-		}));
+		it('should have a function named $outputMixedCards()', function() {
+			expect(angular.isFunction($scope.outputMixedCards)).toBe(true);
+		});
 
-		it('Should do some controller test', inject(function() {
-			getCardsFromAPIs();
-			// ...
-		}));
+	describe('Method: removeHTMLTags()', function() {
+
+		it('should have a function named $removeHTMLTags()', function() {
+			expect(angular.isFunction($scope.removeHTMLTags)).toBe(true);
+		});
 	});
-}());
+	describe('Method: shortenText()', function() {
+
+		it('should have a function named $shortenText()', function() {
+			expect(angular.isFunction($scope.shortenText)).toBe(true);
+		});
+	});
+});
